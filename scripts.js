82767032b85db971d0bbc7a9926f5a17119fd04d
thomasin-abraham <thomasinabraham@me.com>
2017-04-06 14:24:26 +1200

    HEBF

--- scripts.js
+++ scripts.js
@@ -3,19 +3,26 @@ document.addEventListener('DOMContentLoaded', displayPlage)
 function displayPlage () {
   displayQuote()
   document.getElementById("newquote").addEventListener("click", displayQuote)
+    document.getElementById("tweetquote").addEventListener("click", window.open("http://www.google.com"))
 }
 
 function displayQuote() {
 var quoteRequest = new XMLHttpRequest();
 quoteRequest.open('GET', 'https://eda-te-reo.herokuapp.com/api/proverbs', true);
 quoteRequest.send()
-
 quoteRequest.onload = function() {
   if (quoteRequest.readyState === XMLHttpRequest.DONE) {
 jsonObj = JSON.parse(quoteRequest.responseText);
 console.log (jsonObj);
 document.getElementById('maori').innerHTML=jsonObj.source;
 document.getElementById('english').innerHTML=jsonObj.translation;
+quoteLoaded()
 }
 }
+
+}
+
+function quoteLoaded() {
+  maoritext = jsonObj.source
+  console.log(maoritext)
 }
